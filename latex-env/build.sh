2752cee0000535d47bacaff3a6405883e78e6f81
EungGu Yun <yuneg11@gmail.com>
2024-05-15 21:02:31 +0900

    Add latex-env:20240515

--- latex-env/build.sh
+++ latex-env/build.sh
@@ -3,6 +3,4 @@ DATE=$(date -u +%Y%m%d)
 echo "Building latex-env:$DATE ..."
 
 # Build
-docker build -t ghcr.io/yuneg11/latex-env:$DATE - \
-    --build-arg DATE=$DATE \
-    < Dockerfile
+docker build -t ghcr.io/yuneg11/latex-env:$DATE - < Dockerfile
